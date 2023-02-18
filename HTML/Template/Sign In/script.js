cb0adf54edc2014d3e2fcea7e5264ae365620c77
Bhavik Patel <patelbhavik3797@gmail.com>
2023-02-18 19:18:59 +0530

    Commit Done
    18/02/2023 7:30PM

---
+++ HTML/Template/Sign In/script.js
@@ -0,0 +1,6 @@
+function getData()
+{
+    let username = document.getElementById("uname").value;
+    let password = document.getElementById("upass").value;
+    window.alert("Hello " + username);
+}
